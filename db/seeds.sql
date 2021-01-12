c658a26b9cb75d6de2e1f1bc82aab2c5c82ac93e
Dave Powell <evadllewop@gmail.com>
2021-01-12 12:31:37 -0600

    folder/file structure complete

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheese", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Swiss", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Double", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Barbeque Bacon", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("California Avacado", true);
