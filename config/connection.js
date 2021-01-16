1fa104e5f7fa86c5b0bed5a1367fa4f9c6d31895
Dave Powell <evadllewop@gmail.com>
2021-01-16 08:49:04 -0600

    adding 'process.env.JAWSDB_URL' for heroku deployment'

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,20 @@
 require('dotenv').config();
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
 
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATA,
-});
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_DATA,
+    });
+};
 
 // Make connection.
 connection.connect(function (err) {
